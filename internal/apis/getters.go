00bad6a75a50c0807d7d42ee8f6656904ee3e3ab
buster_daemon <bugsbunnygdi@pm.me>
2024-01-17 23:07:32 +0300

    Added parameters for getting random images

--- internal/apis/getters.go
+++ internal/apis/getters.go
@@ -15,6 +15,27 @@ import (
 func getRandFile(ctx *fiber.Ctx) error {
 	name := ctx.Query("name", "")
 	sLandscape := ctx.Query("orientation", "0")
+	sXdim := ctx.Query("xdim", "0")
+	sYdim := ctx.Query("ydim", "0")
+	sXCompar := ctx.Query("xcompar", "0")
+	sYCompar := ctx.Query("ycompar", "0")
+
+	xdim, err := strconv.ParseUint(sXdim, 10, 64)
+	if err != nil {
+		return ctx.SendStatus(http.StatusInternalServerError)
+	}
+	ydim, err := strconv.ParseUint(sYdim, 10, 64)
+	if err != nil {
+		return ctx.SendStatus(http.StatusInternalServerError)
+	}
+	xCompar, err := strconv.ParseUint(sXCompar, 10, 8)
+	if err != nil {
+		return ctx.SendStatus(http.StatusInternalServerError)
+	}
+	yCompar, err := strconv.ParseUint(sYCompar, 10, 8)
+	if err != nil {
+		return ctx.SendStatus(http.StatusInternalServerError)
+	}
 
 	landscape, err := strconv.ParseUint(sLandscape, 10, 8)
 	if err != nil {
@@ -22,6 +43,10 @@ func getRandFile(ctx *fiber.Ctx) error {
 	}
 	f, err := database.SelectRandomFile(globConf.DBPath, database.RandomParams{
 		Substring: name,
+		Xdim:      uint(xdim),
+		XCompar:   uint8(xCompar),
+		Ydim:      uint(ydim),
+		YCompar:   uint8(yCompar),
 		Landscape: uint8(landscape),
 	})
 	if err != nil {
