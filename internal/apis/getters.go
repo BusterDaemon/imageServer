dcfabe435ba399d40ae92d6ecd9c5f5937c5bfba
Buster Daemon <bugsbunnygdi@pm.me>
2024-10-01 12:26:50 +0300

    Added Content-Disposition response header to random file request

--- internal/apis/getters.go
+++ internal/apis/getters.go
@@ -10,24 +10,39 @@ import (
 
 func getRandFile(ctx *fiber.Ctx) error {
 	var (
-		db        *gorm.DB    = ctx.Locals("db").(*gorm.DB)
-		logs      *zap.Logger = ctx.Locals("logger").(*zap.Logger)
-		imagePath string
+		db    *gorm.DB    = ctx.Locals("db").(*gorm.DB)
+		logs  *zap.Logger = ctx.Locals("logger").(*zap.Logger)
+		image             = struct {
+			filePath      string
+			fileName      string
+			fileExtension string
+		}{}
 	)
-	res := db.Select("file_path").
+	res := db.Select("file_path", "format", "hash").
 		Find(&database.Images{}).
 		Order("RANDOM()").
 		Limit(1)
 
 	row := res.Row()
 
-	err := row.Scan(&imagePath)
+	err := row.Scan(
+		&image.filePath,
+		&image.fileExtension,
+		&image.fileName,
+	)
 	if err != nil {
 		logs.Error(err.Error())
 		return ctx.SendStatus(fiber.StatusInternalServerError)
 	}
 
-	return ctx.SendFile(imagePath, true)
+	ctx.Set(
+		"Content-Disposition",
+		"inline;filename=\""+
+			image.fileName+image.fileExtension+
+			"\"",
+	)
+
+	return ctx.SendFile(image.filePath)
 }
 
 func searchImages(ctx *fiber.Ctx) error {
