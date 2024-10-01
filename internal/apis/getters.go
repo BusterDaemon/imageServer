e972eb3cf4dc17ed8e6b25a5e2437db59501bf6a
Buster Daemon <bugsbunnygdi@pm.me>
2024-10-01 12:05:36 +0300

    Added getting random images from database

--- internal/apis/getters.go
+++ internal/apis/getters.go
@@ -1,11 +1,33 @@
 package apis
 
 import (
+	"buster_daemon/imageserver/internal/apis/database"
+
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 func getRandFile(ctx *fiber.Ctx) error {
-	return ctx.SendStatus(fiber.StatusOK)
+	var (
+		db        *gorm.DB    = ctx.Locals("db").(*gorm.DB)
+		logs      *zap.Logger = ctx.Locals("logger").(*zap.Logger)
+		imagePath string
+	)
+	res := db.Select("file_path").
+		Find(&database.Images{}).
+		Order("RANDOM()").
+		Limit(1)
+
+	row := res.Row()
+
+	err := row.Scan(&imagePath)
+	if err != nil {
+		logs.Error(err.Error())
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return ctx.SendFile(imagePath, true)
 }
 
 func searchImages(ctx *fiber.Ctx) error {
